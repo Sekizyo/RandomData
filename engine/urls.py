1cf353aefba8e45f9e0136ae64dd393d314d3d6f
Sekizyo <mateuszpieczulis@gmail.com>
2022-06-20 21:03:30 +0200

    for fuck sake

--- engine/urls.py
+++ engine/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-import hello.views
+import engine.views
 
 # To add a new path, first import the app:
 # import blog
@@ -11,5 +11,5 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.index, name="index")
+    path("", engine.views.index, name="index")
 ]
